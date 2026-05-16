ce2a7077b11ab7b64ca4a77586960e919303cb47
asifboma <bomasair4@gmail.com>
2026-05-17 03:53:07 +0600

    update taskdetails

---
+++ Final/Final task/controllers/taskDetailsController.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once __DIR__ . "/../config/db.php";
+require_once __DIR__ . "/../models/TaskModel.php";
+require_once __DIR__ . "/../models/ProjectModel.php";
+require_once __DIR__ . "/../models/CommentModel.php";
