490bf3223dc271f600336d9190ac54850723f1b7
asifboma <bomasair4@gmail.com>
2026-05-17 03:31:23 +0600

    CommentModel first test

--- Final/Final task/models/CommentModel.php
+++ Final/Final task/models/CommentModel.php
@@ -9,3 +9,56 @@ class CommentModel
         $this->conn = $connection;
     }
 
+    public function addComment($task_id, $user_id, $body)
+    {
+        $sql = "INSERT INTO comments(task_id, user_id, body)
+                VALUES('$task_id', '$user_id', '$body')";
+
+        $result = $this->conn->query($sql);
+
+        if($result)
+        {
+            return $this->conn->insert_id;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public function getCommentsByTask($task_id)
+    {
+        $sql = "SELECT comments.*, users.name
+                FROM comments, users
+                WHERE comments.user_id = users.id
+                AND comments.task_id = '$task_id'
+                ORDER BY comments.created_at ASC";
+
+        $result = $this->conn->query($sql);
+
+        return $result;
+    }
+
+    public function getCommentById($comment_id)
+    {
+        $sql = "SELECT * FROM comments
+                WHERE id = '$comment_id'";
+
+        $result = $this->conn->query($sql);
+
+        return $result;
+    }
+
+    public function deleteComment($comment_id, $user_id)
+    {
+        $sql = "DELETE FROM comments
+                WHERE id = '$comment_id'
+                AND user_id = '$user_id'";
+
+        $result = $this->conn->query($sql);
+
+        return $result;
+    }
+}
+
+?>
